067c87f02e96d106853b6a3f2f7d2c7442650f7b
RayRayHub <160684477+RayRayHub@users.noreply.github.com>
2024-02-20 20:19:26 -0500

    Create Min-Max Sum.cs

---
+++ One month preparation kit/Week 1/Min-Max Sum.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long high = arr.Max(); // Get the highest value in arr.
+        long low = arr.Min(); // Get the lowest value in arr.
+        long total = 0; // Initialize variable that will store the sum as we iterate below.
+        arr.ForEach(x => total += x); //Sum() here took too long as opposed to iterating through this arr
+        long minSum = total - high;
+        long maxSum = total - low;
+        
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
